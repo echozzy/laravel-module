bfdcfc53abb76f627c9185349c879189c2fdf136
echozzy <348858954@qq.com>
2019-09-20 16:48:52 +0800

    zzy config

--- src/config/menus.php
+++ src/config/menus.php
@@ -1,29 +1,42 @@
 <?php
 /** .-------------------------------------------------------------------
- * |      Site: www.zhouzy365.com
- * |      Date: 2019/9/10 下午3:13
- * |    Author: zzy <348858954@qq.com>
+ * |      Site: www.hdcms.com  www.houdunren.com
+ * |      Date: 2018/7/2 上午12:54
+ * |    Author: 向军大叔 <2300071698@qq.com>
  * '-------------------------------------------------------------------*/
 /**
  * 后台菜单配置
  * 下面是属性说明：
  * title 菜单栏目
- * list_order 排序
- * p_id 父级ID
- * icon 图标参考 http://www.fontawesome.com.cn/faicons/
+ * icon 图标参考 http://fontawesome.dashgame.com/
  * menus 子菜单
  * permission 权限标识，必须在permission.php配置文件中存在
  */
 return [
     [
         "title"      => "系统管理",
-        "list_order" => 0,
-        "p_id"       => 0,
-        "icon"       => "fa fa-navicon",
-        'permission' => '权限标识',
+        "p_id"        => 0,//父级ID
+        "icon"       => "fa fa-cogs",
+        'permission' => 'Admin',
         "url" => "链接地址",
         "menus"      => [
-            ["title" => "网站配置", "list_order" => 10, "icon"=> "fa fa-navicon", "permission" => "权限标识", "url" => "链接地址"],
+            ["title" => "网站信息", "icon"=> "fa fa-navicon","permission" => "Admin::config-site", "url" => "链接地址"],
+            ["title" => "邮箱配置", "icon"=> "fa fa-navicon","permission" => "Admin::config-email", "url" => "链接地址"],
+            ["title" => "友情链接", "icon"=> "fa fa-navicon","permission" => "Admin::config-link", "url" => "链接地址"],
+            ["title" => "后台菜单", "icon"=> "fa fa-navicon","permission" => "Admin::config-menu", "url" => "/admin/menu"],
+        ],
+    ],
+    [
+        "title"      => "权限管理",
+        "p_id"        => 0,//父级ID
+        "icon"       => "fas fa-user-shield",
+        'permission' => 'Permission',
+        "url" => "链接地址",
+        "menus"      => [
+            ["title" => "管理员列表", "icon"=> "fa fa-navicon", "permission" => "Permission::list", "url" => "链接地址"],
+            ["title" => "管理员日志", "icon"=> "fa fa-navicon", "permission" => "Permission::log", "url" => "链接地址"],
+            ["title" => "角色管理", "icon"=> "fa fa-navicon", "permission" => "Permission::role", "url" => "/admin/role"],
+            ["title" => "权限列表", "icon"=> "fa fa-navicon", "permission" => "Permission::permissions", "url" => "/admin/permission"],
         ],
     ],
 ];
--- src/config/permission.php
+++ src/config/permission.php
@@ -10,15 +10,27 @@
  */
 return [
     [
-        'title' => '权限管理',
+        'title' => '系统管理',
         'p_id' => 0,
-        'name' => 'Modules\Admin\Http\Controllers\RoleController',
+        'name' => 'Admin',
         'guard_name' => 'admin',
         'permissions' => [
-            ['title' => '管理员列表', 'name' => 'Modules\Admin\Http\Controllers\RoleController@list', 'guard_name' => 'admin'],
-            ['title' => '管理员日志', 'name' => 'Modules\Admin\Http\Controllers\RoleController@log', 'guard_name' => 'admin'],
-            ['title' => '角色管理', 'name' => 'Modules\Admin\Http\Controllers\RoleController@role', 'guard_name' => 'admin'],
-            ['title' => '权限列表', 'name' => 'Modules\Admin\Http\Controllers\RoleController@permission', 'guard_name' => 'admin']
+            ['title' => '网站信息', 'name' => 'Admin::config-site', 'guard_name' => 'admin'],
+            ['title' => '邮箱配置', 'name' => 'Admin::config-email', 'guard_name' => 'admin'],
+            ['title' => '友情链接', 'name' => 'Admin::config-link', 'guard_name' => 'admin'],
+            ['title' => '后台菜单', 'name' => 'Admin::config-menu', 'guard_name' => 'admin']
+        ],
+    ],
+    [
+        'title' => '权限管理',
+        'p_id' => 0,
+        'name' => 'Permission',
+        'guard_name' => 'admin',
+        'permissions' => [
+            ['title' => '管理员列表', 'name' => 'Permission::list', 'guard_name' => 'admin'],
+            ['title' => '管理员日志', 'name' => 'Permission::log', 'guard_name' => 'admin'],
+            ['title' => '角色管理', 'name' => 'Permission::role', 'guard_name' => 'admin'],
+            ['title' => '权限列表', 'name' => 'Permission::permissions', 'guard_name' => 'admin']
         ],
     ],
 ];
